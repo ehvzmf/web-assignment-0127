1390299f68c54be1835efd193d70f07593a65cd5
Yuna <yuna00708@gmail.com>
2025-01-27 23:17:45 +0900

    chore: file orgarnization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Finder from './pages/Finder';
+import Manager from './pages/Manager';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Finder} />
+        <Route path="/manager" component={Manager} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
